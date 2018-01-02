c19c440e4b075f435ae5801cb876d50b8951073c
Clayton Davis <claytondavis1991@gmail.com>
2018-01-02 15:01:37 -0600

    removing pin. Adding row to beersdrank table

--- addBeer/addBeer.py
+++ addBeer/addBeer.py
@@ -3,6 +3,7 @@ import json
 import os
 import re
 import time
+import datetime
 import ast
 from botocore.client import Config
 from botocore.exceptions import ClientError
@@ -14,7 +15,7 @@ def respond(err, res=None):
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST,OPTIONS'
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
         },
     }
 
@@ -32,11 +33,11 @@ def lambda_handler(event, context):
     for row in response['Items']:
         print(row['Name']['S'])
         if row['Name']['S'] == bodyJson['Name']:
-            print(row['PIN']['N'])
-            if row['PIN']['N'] == bodyJson['PIN']:
-                beerCount = int(row['BeerCount']['N'])
-                print(beerCount)
-                processUpdate = True
+    #        print(row['PIN']['N'])
+    #        if row['PIN']['N'] == bodyJson['PIN']:
+            beerCount = int(row['BeerCount']['N'])
+    #            print(beerCount)
+    #            processUpdate = True
 
     beerCount = beerCount + 1
 
@@ -58,5 +59,26 @@ def lambda_handler(event, context):
         TableName='DavisBarApp',
         UpdateExpression='SET #B = :b'
     )
+    print("RESPONSE1")
+    print(response)
+
+    now = datetime.datetime.now()
+    response2 = client.put_item(
+        Item={
+            'Timestamp': {
+                'S': str(now),
+            },
+            'Beer': {
+                'S': bodyJson['Beer'],
+            },
+            'Person': {
+                'S': bodyJson['Name'],
+            },
+        },
+        ReturnConsumedCapacity='TOTAL',
+        TableName='DavisBarApp-BeersDrank',
+    )
+    print("RESPONSE2")
+    print(response2)
 
     return respond(None, response)
